a459da5b563d59e714e263f8ab7d6c79e2114fa7
QY-W <71271157+QY-W@users.noreply.github.com>
2023-04-02 17:19:47 -0500

    add button description

--- view.js
+++ view.js
@@ -35,7 +35,8 @@ function make_file_element(file_obj) {
             <h3 class="selection" id="${file_hash}_header">
                 <span class="oi oi-folder" id="${file_hash}_icon"/> ${file_obj.filename} 
                 <button class="ui-button ui-widget ui-corner-all permbutton" path="${file_hash}" id="${file_hash}_permbutton"> 
-                    <span class="oi oi-lock-unlocked" id="${file_hash}_permicon"/> 
+                    <span class="oi oi-lock-unlocked" id="${file_hash}_permicon"/>
+                    Edit Permission
                 </button>
             </h3>
         </div>`)
@@ -55,7 +56,8 @@ function make_file_element(file_obj) {
         return $(`<div class='file selection'  id="${file_hash}_div">
             <span class="oi oi-file" id="${file_hash}_icon"/> ${file_obj.filename}
             <button class="ui-button ui-widget ui-corner-all permbutton" path="${file_hash}" id="${file_hash}_permbutton"> 
-                <span class="oi oi-lock-unlocked" id="${file_hash}_permicon"/> 
+                <span class="oi oi-lock-unlocked" id="${file_hash}_permicon"/>
+                Edit Permission
             </button>
         </div>`)
     }
